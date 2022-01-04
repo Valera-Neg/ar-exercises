9adc2259cf0b3fe8cdd98a1ccbbd079424ab369f
Valeriy Nehovora <valeriynegovora@gmail.com>
2022-01-04 02:57:46 +0000

    implementing Exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,16 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
+@store3 = Store.where(id: 3)
+p @store3
+
+@store3.destroy_all
+
+
+number_of_stors = Store.count
+p number_of_stors
+
+
+
+
 # Your code goes here ...
